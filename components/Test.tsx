eb4b5076fb660e63721df018f31fd633da20bf12
Amir Ali <amiraliazimloo123@gmail.com>
2024-02-07 23:38:45 +0330

    remove require auth

--- components/Test.tsx
+++ components/Test.tsx
@@ -9,49 +9,49 @@ const Test = () => {
     const pathname = usePathname()
     const { changeName } = useContext(TestContext)
 
-    const checkUserLoggedIn = async()=>{
-        try {
-            console.log(router,"router")
-            console.log(pathname,"pathname")
-            console.log(pathname?.toString()?.split("/")?.[2],"pathname2")
-            const user = await axios.post("/api/me");
-            console.log("dkjhgdsh",user)
+    // const checkUserLoggedIn = async()=>{
+    //     try {
+    //         console.log(router,"router")
+    //         console.log(pathname,"pathname")
+    //         console.log(pathname?.toString()?.split("/")?.[2],"pathname2")
+    //         const user = await axios.post("/api/me");
+    //         console.log("dkjhgdsh",user)
 
-            if(!!user){
-                changeName(user.data.user.name)
-            }
+    //         if(!!user){
+    //             changeName(user.data.user.name)
+    //         }
 
-            if(!!user 
-                && (
-                pathname?.toString()?.split("/")?.[2] == "register"
-                ||
-                pathname?.toString()?.split("/")?.[2] == "login"
-                ||
-                pathname?.toString()?.split("/")?.[2] == "checkotp"
-            )){
-                changeName(user.data.user.name)
-                router.push("/")
-            }
-            console.log("user =>",user)
-        } catch (error) {
-            console.log(error)
-            if(
-                pathname?.toString()?.split("/")?.[2] !== "register"
-                &&
-                pathname?.toString()?.split("/")?.[2] !== "login"
-                &&
-                pathname?.toString()?.split("/")?.[2] !== "checkotp"
-                ){
-                    router.push("/login")
-            }
-        }
-    }
+    //         if(!!user 
+    //             && (
+    //             pathname?.toString()?.split("/")?.[2] == "register"
+    //             ||
+    //             pathname?.toString()?.split("/")?.[2] == "login"
+    //             ||
+    //             pathname?.toString()?.split("/")?.[2] == "checkotp"
+    //         )){
+    //             changeName(user.data.user.name)
+    //             router.push("/")
+    //         }
+    //         console.log("user =>",user)
+    //     } catch (error) {
+    //         console.log(error)
+    //         if(
+    //             pathname?.toString()?.split("/")?.[2] !== "register"
+    //             &&
+    //             pathname?.toString()?.split("/")?.[2] !== "login"
+    //             &&
+    //             pathname?.toString()?.split("/")?.[2] !== "checkotp"
+    //             ){
+    //                 router.push("/login")
+    //         }
+    //     }
+    // }
 
-    useEffect(()=>{
+    // useEffect(()=>{
 
-       (async()=>await checkUserLoggedIn())()
+    //    (async()=>await checkUserLoggedIn())()
 
-    },[router,pathname])
+    // },[router,pathname])
 
     return ( 
         <div>
