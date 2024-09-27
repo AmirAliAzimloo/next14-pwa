1a3987f502d48c6552dbbbdd8dc23e52615dd1e2
Amir Ali <amiraliazimloo123@gmail.com>
2024-09-27 22:14:17 +0330

    Add the offline page.

---
+++ app/offline/page.tsx
@@ -0,0 +1,8 @@
+
+export default function OfflinePage() {
+    return (
+      <main className='w-full h-screen grid place-items-center'>
+      <h1 className='text-3xl font-bold'>You are offline</h1>
+    </main>
+    )
+  }
--- next.config.js
+++ next.config.js
@@ -7,8 +7,15 @@ const withPWA = require("@ducanh2912/next-pwa").default({
     reloadOnOnline:true,
     swcMinify:true,
     disable:false,
+    fallbacks: {
+        //image: "/static/images/fallback.png",
+        document: "/offline", // if you want to fallback to a custom page rather than /_offline
+        // font: '/static/font/fallback.woff2',
+        // audio: ...,
+        // video: ...,
+    },
     workboxOptions:{
-        disableDevLogs: true
+        disableDevLogs: true,
     }
 });
 
